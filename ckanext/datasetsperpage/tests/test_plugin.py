84833ebbc34f2c9d7f4d7aabc5881d6049f21622
pdelboca <patriciodelboca@gmail.com>
2019-04-04 15:31:18 -0300

    First tests added.

--- ckanext/datasetsperpage/tests/test_plugin.py
+++ ckanext/datasetsperpage/tests/test_plugin.py
@@ -1,5 +1,25 @@
 """Tests for plugin.py."""
-import ckanext.datasetsperpage.plugin as plugin
+# import ckanext.datasetsperpage.plugin as plugin
+import ckan.plugins
+from ckan.tests.helpers import call_action
+from nose.tools import assert_equal
 
-def test_plugin():
-    pass
+
+class TestDataSetPerPagePlugin(object):
+    """Tests for datasetperpage plugin."""
+
+    @classmethod
+    def setup_class(cls):
+        """Nose runs this method once to setup our test class."""
+        if not ckan.plugins.plugin_loaded('datasetsperpage'):
+            ckan.plugins.load('datasetsperpage')
+
+    @classmethod
+    def teardown_class(cls):
+        """Nose Runs after all the test methods in our class have been run."""
+        ckan.plugins.unload('datasetsperpage')
+
+    def test_datasets_per_page_is_editable_runtime(self):
+        """Run."""
+        configs = call_action('config_option_list')
+        assert_equal('ckan.datasets_per_pagess' in configs, True)
