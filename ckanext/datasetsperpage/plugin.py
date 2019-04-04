0e4937e06f1353fcbde72754285aa7134fd12b14
pdelboca <patriciodelboca@gmail.com>
2019-04-04 10:23:34 -0300

    Logic migrated from old repo.

--- ckanext/datasetsperpage/plugin.py
+++ ckanext/datasetsperpage/plugin.py
@@ -5,9 +5,19 @@ import ckan.plugins.toolkit as toolkit
 class DatasetsperpagePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
 
-    # IConfigurer
+    def update_config(self, config):
+        """Add extension templates directory."""
+        toolkit.add_template_directory(config, 'templates')
 
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'datasetsperpage')
+    def update_config_schema(self, schema):
+        """Make datasets_per_page editable at runtime."""
+        ignore_missing = toolkit.get_validator('ignore_missing')
+        is_positive_integer = toolkit.get_validator('is_positive_integer')
+
+        schema.update({
+            # This is an existing CKAN core configuration option, we are just
+            # making it available to be editable at runtime
+            'ckan.datasets_per_page': [ignore_missing, is_positive_integer],
+        })
+
+        return schema
